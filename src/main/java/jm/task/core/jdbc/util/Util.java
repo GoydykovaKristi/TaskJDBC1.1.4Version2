d6662ee8c6a7b1d9e7fe40dbdbdb2fd01b1f9f0b
GoydykovaKristi <K4154492@yandex.ru>
2021-03-01 06:04:52 +0300

    1.1.4Version2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -16,26 +16,26 @@ public class Util {
             // Настройки hibernate
             Configuration configuration = new Configuration()
                     .setProperty( "hibernate.connection.driver_class",
-                            "com.mysql.cj.jdbc.Driver" )//
+                            "com.mysql.cj.jdbc.Driver" )
                     .setProperty( "hibernate.connection.url",
-                            "jdbc:mysql://127.0.0.1:3306/mydbtest?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false" )//
+                            "jdbc:mysql://127.0.0.1:3306/mydbtest?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false" )
                     .setProperty( "hibernate.connection.username",
-                            "bestuser" )//
+                            "bestuser" )
                     .setProperty( "hibernate.connection.password",
-                            "bestuser" )//
-                    .setProperty( "hibernate.connection.pool_size", "1" )
-                    .setProperty( "hibernate.connection.autocommit", "false" )
-                    .setProperty( "hibernate.cache.provider_class",
-                            "org.hibernate.cache.NoCacheProvider" )
-                    .setProperty( "hibernate.cache.use_second_level_cache",
-                            "false" )
-                    .setProperty( "hibernate.cache.use_query_cache", "false" )
+                            "bestuser" )
+//                    .setProperty( "hibernate.connection.pool_size", "1" )
+//                    .setProperty( "hibernate.connection.autocommit", "false" )
+//                    .setProperty( "hibernate.cache.provider_class",
+//                            "org.hibernate.cache.NoCacheProvider" )
+//                    .setProperty( "hibernate.cache.use_second_level_cache",
+//                            "false" )
+//                    .setProperty( "hibernate.cache.use_query_cache", "false" )
                     .setProperty( "hibernate.dialect",
-                            "org.hibernate.dialect.MySQLDialect" )//
-                    .setProperty( "hibernate.show_sql","true" )//
+                            "org.hibernate.dialect.MySQLDialect" )
+                    .setProperty( "hibernate.show_sql","true" )
                     .setProperty( "hibernate.current_session_context_class",
-                            "thread" )//
-                    .setProperty("hibernate.hbm2ddl.auto", "update")//
+                            "thread" )
+                    .setProperty("hibernate.hbm2ddl.auto", "update")
 
                     .addPackage( "ru.mysql.db" )
                     .addAnnotatedClass(User.class);
@@ -48,6 +48,4 @@ public class Util {
         }
         return sessionFactory;
     }
-
-
 }
